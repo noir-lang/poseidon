54e490abeec50d08c396dd4f47e16a15100d405f
github-action-benchmark <github@users.noreply.github.com>
2026-01-12 10:38:01 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for 8072f53dd63769eadb7815da47f6ea683a3ee272

---
+++ dev/bench/data.js
@@ -0,0 +1,107 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768214281334,
+  "repoUrl": "https://github.com/noir-lang/poseidon",
+  "entries": {
+    "ACIR Opcodes": [
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8072f53dd63769eadb7815da47f6ea683a3ee272",
+          "message": "chore: update benchmark.yml with deployment permissions (#24)",
+          "timestamp": "2026-01-12T10:37:47Z",
+          "tree_id": "9135545e867b27e70075d331ead4b24d81ac6ae7",
+          "url": "https://github.com/noir-lang/poseidon/commit/8072f53dd63769eadb7815da47f6ea683a3ee272"
+        },
+        "date": 1768214280924,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "poseidon2_hash_1.json/main",
+            "value": 4,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon2_hash_10.json/main",
+            "value": 13,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon2_hash_2.json/main",
+            "value": 4,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon2_hash_20.json/main",
+            "value": 26,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon2_hash_4.json/main",
+            "value": 5,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon2_hash_8.json/main",
+            "value": 10,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_hash_1.json/main",
+            "value": 335,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_hash_2.json/main",
+            "value": 318,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_hash_4.json/main",
+            "value": 392,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_hash_8.json/main",
+            "value": 528,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_sponge_1.json/main",
+            "value": 379,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_sponge_16.json/main",
+            "value": 1592,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_sponge_32.json/main",
+            "value": 3192,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_sponge_4.json/main",
+            "value": 392,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "poseidon_sponge_8.json/main",
+            "value": 792,
+            "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ]
+  }
+}
